9efeef3d86e52083590d2ad46e0d46b9fa6c34e7
site-demo <site9demo@monumentmail.com>
2015-08-08 16:12:52 -0500

    updating test configuration file

--- protractor_conf.js
+++ protractor_conf.js
@@ -1,20 +1,21 @@
 // An example configuration file.
 exports.config = {
-  seleniumAddress: 'http://0.0.0.0:4444/wd/hub',
+  directConnect: true,
+
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
     'browserName': 'chrome'
   },
 
-  baseUrl: 'http://127.0.0.1:9000/',
+  // Framework to use. Jasmine 2 is recommended.
+  framework: 'jasmine2',
 
   // Spec patterns are relative to the current working directly when
   // protractor is called.
-  specs: ['test/e2e/**/*.spec.js'],
+  specs: ['test/e2e/**/*_spec.js'],
 
-  // Options to be passed to Jasmine-node.
+  // Options to be passed to Jasmine.
   jasmineNodeOpts: {
-    showColors: true,
     defaultTimeoutInterval: 30000
   }
 };
